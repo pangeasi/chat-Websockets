578bda0ac630368d06a28ed38cd3db6105caa19f
gammafp <gammafp@gmail.com>
2018-07-25 17:26:42 +0200

    Se ha creado la estructura para hacer el socket.
    - Se crearon las carpetas web y server. 
    En la carpeta server se encuentra el archivo init.js el cual se puede ejecutar con node init.js
    - La carpeta web está el index.html que se puede ejecutar sin mas.

---
+++ server/init.js
@@ -0,0 +1,9 @@
+const net = require("net");
+
+net.createServer((socket) => {
+
+})
+.listen({
+    host: "localhost",
+    port: 3030
+}, () => console.log("Se ha iniciado el servidor en el puerto 3030"));
---
+++ web/js/main_server.js
@@ -0,0 +1,32 @@
+// Hay que modificar este socket por el siguiente: 
+// var wsUri = "ws://localhost:3030";
+
+var wsUri = "wss://echo.websocket.org";
+
+var output = document.getElementById("output");
+var notificador = document.getElementById("notificador");
+
+function init() {
+    testWebSocket();
+}
+
+function testWebSocket() {
+    websocket = new WebSocket(wsUri);
+    websocket.addEventListener("open", (event) => {
+        notificador.style.background = "#3498db";
+        output.innerHTML = "<span style='color:#3498db'>Conectado</span>";
+    });
+    websocket.addEventListener("message", (event) => {
+        output.innerHTML += "<span style='color:#e74c3c'>Error Conexión</span>";
+        notificador.style.background = "#e74c3c";
+    });
+    websocket.addEventListener("error", (event) => {
+        output.innerHTML += '<span>Error conexión</span>';
+        notificador.style.background = '#e74c3c';
+    });
+    websocket.addEventListener("close", (event) => {
+        output.innerHTML = "<br><hr><span style='color: #f1c40f'>Conexión cerrada</span>"
+    });
+}
+
+window.addEventListener("load", init, false);
