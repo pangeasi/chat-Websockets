a31c312a5cb5bfe9f998ac8523aa3542fec57421
Magdiel Lopez <lopmag.lopez@gmail.com>
2018-07-30 00:02:24 -0600

    comunicacion cliente - servidor

--- chat/init.js
+++ chat/init.js
@@ -12,4 +12,17 @@ app.use(express.static('public'));
 
 app.get('/home', (req, res)=>res.sendFile('public/index.html' , { root : __dirname}));
 
+/*    sockets    */
+io.on('connection', function(socket){
+  socket.on('new user', function(data){
+    users.push({name:data.name,pass:data.pass});
+    socket.emit('getUsers', users);
+    socket.emit('getMensajes', chats);
+  });
+  socket.on('nuevo', function(data){
+    chats.push({name:data.name,msj:data.msj});
+    socket.emit('add', chats[chats.length-1]);
+  });
+});
+
 server.listen(port,()=>console.log('Servidor corriendo en http://localhost:'+port));
--- chat/public/js/main.js
+++ chat/public/js/main.js
@@ -1,30 +1,38 @@
 const socket = io.connect('http://localhost:3030', { 'forceNew': true });
-let user = {name:'',pass:''};
-function init() {
-    testWebSocket();
-}
+const userName = document.getElementById('nombre');
+const userPass = document.getElementById('pass');
+const acceder = document.getElementById('btn-acceder');
+const uconectados = document.getElementById('uconectados');
+const ucuenta = document.getElementById('cuenta');
+const enviar = document.getElementById('enviar');
+const textoMensaje = document.getElementById('msj-text');
+const mensajes = document.getElementById('mensajes');
+const msjInit = document.getElementsByTagName('label')[0];
 
-
-const escribiendo = ()=>socket.emit('escribiendo', user.name);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+acceder.addEventListener('click',()=>{
+	socket.emit('new user', {name:userName.value,pass:userPass.value});
+	ucuenta.innerHTML = `<br><h1>${userName.value}</h1>`;
+	msjInit.innerHTML = 'inicio del chat';
+});
+enviar.addEventListener('click',()=>{
+	socket.emit('nuevo', {name:userName.value,msj:textoMensaje.value});
+});
 
 
 
+socket.on('getUsers',(u)=>{
+	let listaUsuarios = '<ul>';
+	for(var i in u){
+		if(u[i].name != userName.value)listaUsuarios+=`<li><span>${u[i].name}</span></li>`;
+	}
+	uconectados.innerHTML = listaUsuarios+'</ul>';
+});
 
-
-
-
-window.addEventListener("load", init, false);
+socket.on('add',(m)=>(m.name == userName.value)?mensajes.innerHTML +=`<div class="textChat me"><b>${m.name}</b><p>${m.msj}</p></div>`:mensajes.innerHTML+=`<div class="textChat"><b>${m.name}</b><p>${m.msj}</p></div>`);
+socket.on('getMensajes',(m)=>{
+	var dt = '';
+	for(var i in m){
+		(m[i].name == userName.value)?dt+=`<div class="textChat me"><b>${m[i].name}</b><p>${m[i].msj}</p></div>`:dt+=`<div class="textChat"><b>${m[i].name}</b><p>${m[i].msj}</p></div>`;
+	}
+	mensajes.innerHTML = dt;
+});
